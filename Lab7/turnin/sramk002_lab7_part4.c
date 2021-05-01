d322a3ad85830d59362315d078e875a382eed3ae
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-05-01 14:27:26 -0700

    Update sramk002_lab7_part4.c

--- Lab7/turnin/sramk002_lab7_part4.c
+++ Lab7/turnin/sramk002_lab7_part4.c
@@ -7,7 +7,7 @@
  *      I acknowledge all content contained herein, excluding template or example
  *      code, is my own original work.
  *
- *      Demo Video: https://youtu.be/Uma1ZXRj6i4
+ *      Demo Video: https://youtu.be/ldOcv_y2CZY
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
